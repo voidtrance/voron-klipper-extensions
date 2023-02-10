374d738eaee948cc1affa4cc255655d24f7a0bb6
Mitko Haralanov <voidtrance@gmail.com>
2023-02-10 11:05:08 -0800

    Fix some issues with led_interpolate extension
    The extension had a couple of issues that needed to be fixed:
    
      1. The act of looking up the LED object could produce a Klipper crash
         if the object does not exist. Fix this by checking of the object is
         in the set of registered LED helpers before dereferencing it.
      2. The way the timer was being rescheduled could lead to it executin
         back-to-back. Time rescheduling is based on the absolute time in
         the future when the timer callback should be called. So, if the
         timer callback took to long to execute, the computed re-trigger
         could be in the past. Fix this by calculating the call time based
         on the current time at the end of the timer callback.
      3. Don't reschedule the timer callback if the printer is shutdown.
    
    This commit also includes a bunch of formatting changes. Since it is not
    part of the Klipper source code base, it doesn't have to comply with the
    Klipper coding style.

--- led_interpolate.py
+++ led_interpolate.py
@@ -7,6 +7,7 @@ import logging
 
 INTERPOLATE_STEP_TIME = 0.1
 
+
 class LedInterpolate:
     def __init__(self, config):
         self.printer = config.get_printer()
@@ -23,9 +24,11 @@ class LedInterpolate:
 
     def _interpolate_leds(self, eventtime):
         all_done = False
+        reactor = self.printer.get_reactor()
         led_count = self.led_helper.get_led_count()
         for index in range(led_count):
-            current_state = [round(c * 255) for c in self.led_helper.led_state[index]]
+            current_state = [round(c * 255)
+                             for c in self.led_helper.led_state[index]]
 
             if current_state == self.interpolation_params[0]:
                 all_done = True
@@ -38,34 +41,38 @@ class LedInterpolate:
             blue = self.compute_color(colors[2], target_colors[2], factor)
             white = self.compute_color(colors[3], target_colors[3], factor)
 
-            self.led_helper.set_color(index, (round(red / 255, 2), round(green / 255, 2),
-                                              round(blue / 255, 2), round(white / 255, 2)))
+            self.led_helper.set_color(index,
+                                      (round(red / 255, 2),
+                                       round(green / 255, 2),
+                                       round(blue / 255, 2),
+                                       round(white / 255, 2)))
             self.current_colors[index] = [red, green, blue, white]
             all_done = False
         self.led_helper.check_transmit(None)
-        if all_done:
-            reactor = self.printer.get_reactor()
+        if all_done or self.printer.is_shutdown():
             return reactor.NEVER
-        return eventtime + INTERPOLATE_STEP_TIME
+        return reactor.monotonic() + INTERPOLATE_STEP_TIME
 
-    def cmd_LED_INTERPOLATE(self, params):
-        target_name = params.get("LED")
-        target_red = round(params.get_float("RED", minval=0.0, maxval=1.0) * 255)
-        target_green = round(params.get_float("GREEN", minval=0.0, maxval=1.0) * 255)
-        target_blue = round(params.get_float("BLUE", minval=0.0, maxval=1.0) * 255)
-        target_white = round(params.get_float("WHITE", 0.0, minval=0.0, maxval=1.0) * 255)
-        factor = params.get_float("FACTOR", 0.3, minval=0.01, maxval=1.0)
+    def cmd_LED_INTERPOLATE(self, gcmd):
+        target_name = gcmd.get("LED")
+        target_colors = [
+            round(gcmd.get_float("RED", minval=0.0, maxval=1.0) * 255),
+            round(gcmd.get_float("GREEN", minval=0.0, maxval=1.0) * 255),
+            round(gcmd.get_float("BLUE", minval=0.0, maxval=1.0) * 255),
+            round(gcmd.get_float("WHITE", 0.0, minval=0.0, maxval=1.0) * 255)]
+        factor = gcmd.get_float("FACTOR", 0.3, minval=0.01, maxval=1.0)
+
+        if target_name not in self.led.led_helpers:
+            raise gcmd.error("Unknown LED object '%s'" % target_name)
 
         self.led_helper = self.led.led_helpers[target_name]
-        if not self.led_helper:
-            logging.exception("Target LED does not exists")
-            return
-
-        self.interpolation_params = ([target_red, target_green, target_blue, target_white],
-                                     factor)
-        self.current_colors = [[round(c * 255, 2) for c in led] for led in self.led_helper.led_state]
+        self.interpolation_params = (target_colors, factor)
+        self.current_colors = [[round(c * 255, 2) for c in led]
+                               for led in self.led_helper.led_state]
         reactor = self.printer.get_reactor()
-        self.timer = reactor.register_timer(self._interpolate_leds, reactor.NOW)
+        self.timer = reactor.register_timer(self._interpolate_leds,
+                                            reactor.NOW)
+
 
 def load_config(config):
     return LedInterpolate(config)
